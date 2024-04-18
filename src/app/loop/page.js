f58d60ee74a46a11753407a22039554257934534
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-04-18 10:48:45 +0200

    added folder structure

---
+++ src/app/loop/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Loop() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-0">
+      Velkommen til Loop, denne siden er under konstruksjon.
+    </main>
+  );
+}
