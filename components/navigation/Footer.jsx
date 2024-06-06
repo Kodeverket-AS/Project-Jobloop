46ad102ecb980dbf15cc948b9ba80bf237cbbb65
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-06-06 13:13:53 +0200

    fixed import path

--- components/navigation/Footer.jsx
+++ components/navigation/Footer.jsx
@@ -2,8 +2,8 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import logoJobloopHvit from "../public/logoJobloopHvit.svg";
-import logoKodeverketHvit from "../public/logoKodeverketHvit.svg";
+import logoJobloopHvit from "../../public/logoJobloopHvit.svg";
+import logoKodeverketHvit from "../../public/logoKodeverketHvit.svg";
 
 // Contact information for the footer
 const contactInformation = [
