b4d09996413c4cee56811105d18e7c5bd6c527c6
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-06-06 13:24:44 +0200

    fixed navbar conditionals

--- components/navigation/Navigation.jsx
+++ components/navigation/Navigation.jsx
@@ -21,57 +21,12 @@ export default function Navigation() {
   const pathname = usePathname();
   let navbar;
 
-  if (pathname === ansettelser) {
-    navbar = <NavbarAlt />;
-  } else if (pathname === about) {
-    navbar = <NavbarAlt />;
-  } else if (pathname === kontakt) {
-    navbar = <NavbarAlt />;
-  } else if (pathname === samarbeidspartner) {
-    navbar = (
-      <>
-        <div className="block md:hidden">
-          <Navbar />
-        </div>
-        <div className="hidden md:block">
-          <NavbarAlt subpage="Samarbeidspartner" />
-        </div>
-      </>
-    );
-  } else if (pathname === arbeidsgiver) {
-    navbar = (
-      <>
-        <div className="block md:hidden">
-          <Navbar />
-        </div>
-        <div className="hidden md:block">
-          <NavbarAlt subpage="Arbeidsgiver" />
-        </div>
-      </>
-    );
-  } else if (pathname === deltaker) {
-    navbar = (
-      <>
-        <div className="block md:hidden">
-          <Navbar />
-        </div>
-        <div className="hidden md:block">
-          <NavbarAlt subpage="Deltaker" />
-        </div>
-      </>
-    );
-  } else if (pathname === visjon) {
-    navbar = (
-      <div>
-        <NavbarAlt />
-      </div>
-    );
-  } else if (pathname === faq) {
-    navbar = <NavbarAlt />;
-  } else {
+  if (pathname === landing) {
     navbar = <Navbar />;
+  } else {
+    navbar = <NavbarAlt />;
   }
-
+   
   return (
     <div>
       {/* 
