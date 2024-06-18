5c5e8201b5123c22ec9b822cf03e7d6bd232aad4
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-06-18 11:18:55 +0200

    created landing hero component

---
+++ components/LandingHero.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function LandingHero() {
+    return (
+        <div>
+            <p>Here comes a hero section</p>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import LandingHero from "../../components/LandingHero";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-0">
+      <LandingHero />      
       Velkommen til jobloop.no, denne siden er under konstruksjon.
     </main>
   );
