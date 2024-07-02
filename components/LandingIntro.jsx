d6bcb1bbb8f4e3f35cf8d41ef4124d4c85947c81
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-07-02 14:07:42 +0200

    completed styling intro section

--- components/LandingIntro.jsx
+++ components/LandingIntro.jsx
@@ -7,13 +7,13 @@ import JobloopPhoto from "../public/JobloopPhoto.png";
 
 export default function LandingIntro() {
   return (
-    <div className="flex flex-col-reverse px-8 py-20 md:px-16 md:py-24 md:flex-row ">
-      <div className="w-full md:w-1/2">
+    <div className="flex flex-col gap-8 px-8 py-20 md:px-16 md:py-24 md:flex-row md:gap-0 ">
+      <div className="w-full md:w-1/2 md:pt-3">
         <Image src={JobloopPhoto} width={500} height={500} />
       </div>
-      <div className="w-full md:w-1/2 pb-7 md:pb-0">
+      <div className="w-full md:w-1/2 pb-7 md:pb-0 md:pl-5 xl:pl-0">
         <h2 className="pb-2 border-b-4 border-jobloop-primary-green w-fit">Hvem er Jobloop?</h2>
-        <p className="pt-8 pb-12 md:pr-36">
+        <p className="pt-5 pb-12 md:pr-0 lg:pr-16">
           JobLoop har oppnådd en imponerende suksessrate siden 2014, som er
           bekreftet av FAFOs forskning. Vi er stolte av å kunne tilby en
           plattform som hjelper diverse grupper med å finne arbeid og tilbyr
