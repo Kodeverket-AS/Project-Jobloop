4cfd75bdd63dcdf52a0a0a3470d695aa06b07356
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-07-17 10:31:30 +0200

    fixed styling success and contact

--- components/ContactComponent.jsx
+++ components/ContactComponent.jsx
@@ -6,14 +6,16 @@ import { LinkButton } from "./Buttons";
 
 export default function ContactComponent() {
   return (
-    <div className="flex flex-col items-center justify-center gap-8 px-8 py-20 md:px-16 md:py-24 md:flex-row-reverse md:gap-0">
+    <div className="flex flex-col items-center justify-center gap-4 px-8 py-20 md:px-16 md:py-24 ">
       <h2 className="pb-2 border-b-4 border-jobloop-primary-green w-fit">
         Kontakt oss
       </h2>
-      <p className="pt-5 pb-12 md:pr-0 lg:pr-16">
+      <p className="pt-5 pb-12 w-fit">
         Trenger du hjelp eller ønsker å samarbeide med oss?
       </p>
-      <LinkButton Path={"/kontakt"} Text={"Les mer"} />
+      <div>
+        <LinkButton Path={"/kontakt"} Text={"Les mer"} />
+      </div>
     </div>
   );
 }
--- components/JobloopSuccess.jsx
+++ components/JobloopSuccess.jsx
@@ -7,12 +7,12 @@ import { ExternalLinkButton } from "./Buttons";
 
 export default function JobloopSuccess() {
   return (
-    <div className="flex flex-col gap-8 px-8 py-20 md:px-16 md:py-24 md:flex-row-reverse md:gap-0 ">
+    <div className="flex flex-col gap-8 px-8 py-20 md:px-16 md:py-24 md:flex-row md:gap-0 ">
       <div className="w-full md:w-1/2 md:pt-3">
         <Image src={KodehodeImage} width={500} height={500} />
       </div>
       <div className="w-full md:w-1/2 pb-7 md:pb-0 md:pl-5 xl:pl-0">
-        <h2 className="pb-2 border-b-4 border-jobloop-primary-green w-fit">
+        <h2 className="pb-2 w-fit">
           Vår suksessrate og FAFOs forskning om effektiviteten siden 2014
         </h2>
         <p className="pt-5 pb-12 md:pr-0 lg:pr-16">
