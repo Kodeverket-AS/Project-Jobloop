e7a7a0124cdbc0c2686df883b96664d868382561
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-07-19 10:33:33 +0200

    fixed error in query

--- lib/GetData.jsx
+++ lib/GetData.jsx
@@ -2,7 +2,7 @@ import { Client } from "./Sanity";
 
 export async function getData() {
   return Client.fetch(`{
-    "tiltak": *[_type == "tiltak"] | order(index) {title, heading, introtext, "image": image.asset->url, targetGroup, targetGroupShort, duration, lectureType, locations, curriculum, use, leader, "image2": image2.asset->url, employer, "image3": image3.asset->url, school, "image4": image4.asset->url, cities, FAQ1, FAA1, FAQ2, FAA2, FAQ3, FAA3, index _id},
+    "tiltak": *[_type == "tiltak"] | order(index) {title, heading, introtext, "image":image.asset->url, targetGroup, targetGroupShort, duration, lectureType, locations, curriculum, use, leader, "image2":image2.asset->url, employer, "image3":image3.asset->url, school, "image4":image4.asset->url, cities, FAQ1, FAA1, FAQ2, FAA2, FAQ3, FAA3, index, _id},
   }`);
   //{ cache: "no-cache" }
 }
