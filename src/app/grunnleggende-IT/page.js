8034c22df671980c4f332aedb4bf22fe0cdae4ad
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-07-24 09:22:53 +0200

    reverting imports

--- src/app/grunnleggende-IT/page.js
+++ src/app/grunnleggende-IT/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { getData } from "../../../lib/GetData";
-import HeroSub from "../../../components/HeroSub";
+import HeroSub from "../../../components/herosub";
 
 export default async function GrunnleggendeIT() {
   const data = await getData();
