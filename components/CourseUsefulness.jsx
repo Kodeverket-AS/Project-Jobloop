d9c43a0d6cf1c951a7691d83ad394f8e00b7a49c
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-07-25 14:13:37 +0200

    removed shadow

--- components/CourseUsefulness.jsx
+++ components/CourseUsefulness.jsx
@@ -10,7 +10,7 @@ export default function CourseUsefullness({ content, course }) {
       <div className="w-1/2">
         <p>Boxes go here</p>
       </div>
-      <div className="flex flex-col items-center justify-center w-1/2 gap-3 px-5 py-10 shadow-xl md:py-5">
+      <div className="flex flex-col items-center justify-center w-1/2 gap-3 px-5 py-10 md:py-5">
         <h2>{"Hva kan du bruke " + course + " til?"}</h2>
         <PortableText value={content.use} />
       </div>
