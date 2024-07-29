d72db58ae5660879db7a2017527a2081cbcb7feb
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-07-29 10:49:24 +0200

    revised image and text alignment

--- components/LeaderSection.jsx
+++ components/LeaderSection.jsx
@@ -7,15 +7,16 @@ import { PortableText } from "@portabletext/react";
 export default function LeaderSection({ content, leader, position }) {
   return (
     <div className="flex flex-row px-8 py-20">
-      <div className="w-full md:w-1/2 md:pt-3">
+      <div className="w-full md:w-96 md:pt-3 md:h-96">
         <Image
           src={content.image2 ? content.image2 : ""}
           width={500}
           height={500}
           alt={content.alt2 ? content.alt2 : ""}
+          className="object-cover w-full h-full"
         />
       </div>
-      <div className="flex flex-col items-center justify-center w-1/2 gap-3 px-5 py-10 md:py-5">
+      <div className="flex flex-col w-1/2 gap-3 px-5 py-10 md:py-5">
         <h2>{position}</h2>
         <p className="pb-3 border-b-2 border-jobloop-primary-green">{leader}</p>
         <PortableText value={content.leader} />
