696c7af0c91511e7d261c7c792ccb5913f845bd2
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-07-29 13:06:51 +0200

    revised alignment

--- components/LeaderSection.jsx
+++ components/LeaderSection.jsx
@@ -6,7 +6,7 @@ import { PortableText } from "@portabletext/react";
 
 export default function LeaderSection({ content, leader, position }) {
   return (
-    <div className="flex flex-row px-8 py-20">
+    <div className="flex flex-row justify-between px-8 py-20">
       <div className="w-full md:w-96 md:pt-3 md:h-96">
         <Image
           src={content.image2 ? content.image2 : ""}
@@ -16,7 +16,7 @@ export default function LeaderSection({ content, leader, position }) {
           className="object-cover w-full h-full"
         />
       </div>
-      <div className="flex flex-col w-1/2 gap-3 px-5 py-10 md:py-5">
+      <div className="flex flex-col w-1/2 gap-3 py-10 md:py-0">
         <h2>{position}</h2>
         <p className="pb-3 border-b-2 border-jobloop-primary-green">{leader}</p>
         <PortableText value={content.leader} />
