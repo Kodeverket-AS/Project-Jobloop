398c75772fe9acb7bfb7ba3769bfc6143fa03f6a
kristoffer <kunkristoffer@gmail.com>
2024-07-31 23:57:44 +0200

    Converting to typescript

--- src/app/kontakt/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Kontakt() {
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-0">
-      Her kommer kontaktsiden, denne siden er under konstruksjon.
-    </main>
-  );
-}
---
+++ src/app/kontakt/page.tsx
@@ -0,0 +1,7 @@
+export default async function Kontakt() {
+  return (
+    <main className="flex flex-col items-center justify-between p-0">
+      ...
+    </main>
+  );
+}
