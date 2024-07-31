5bc97e7b9071cbb6cc37cb6d4793a9ab5f5b4dba
kristoffer <kunkristoffer@gmail.com>
2024-08-01 00:45:25 +0200

    Including component card and placeholder sorting section

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -17,10 +17,25 @@ export default async function Kontakt() {
   const data = await sanityFetch<ContactDetails[]>({query: QUERY});
 
   return (
-    <main className="flex flex-col items-center justify-between p-0">
-      {data.map(contact => (
-        <ContactCard key={contact._id} contact={contact}/>
-      ))}
+    <main className="flex flex-col items-center justify-between py-24">
+      <div className="hidden bg-green-700">
+        <span className="flex flex-col">
+          <label htmlFor="">Søk etter ansatt</label>
+          <input type="text" />
+        </span>
+        <span className="flex flex-col">
+          <label htmlFor="">Avdeling</label>
+          <select>
+            <option value="">Bergen</option>
+            <option value="">Stavanger</option>
+          </select>
+        </span>
+      </div>
+      <div className="flex flex-col gap-12">
+        {data.map(contact => (
+          <ContactCard key={contact._id} contact={contact}/>
+        ))}
+      </div>
     </main>
   );
 }
