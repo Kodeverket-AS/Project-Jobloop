70265fa93dc103f1b55e6e6e9b9778e0bf05e413
kristoffer <kunkristoffer@gmail.com>
2024-08-01 00:44:05 +0200

    Creating contact us component card for rending details

---
+++ components/contacts/Card.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function ContactCard({ contact }) {
+  const { image, fname, lname, role, company, email, mobile } = contact
+  return (
+    <div className="flex gap-24 h-96">
+      <Image className="object-cover object-top" src={image} width={300} height={500} sizes="xl:100vw xxl:100vw xxxl:100vw" alt={`A sexy picture of ${fname}`}/>
+      <div className="flex flex-col justify-center">
+        <h3>{fname} {lname}</h3>
+        <p>{role}, {company}</p>
+        <p>{email}</p>
+        <p>{mobile}</p>
+      </div>
+    </div>
+  );
+}
