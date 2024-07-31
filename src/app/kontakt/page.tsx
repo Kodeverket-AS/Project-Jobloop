84d837a25b911fb903e603723f1455343ce2581f
kristoffer <kunkristoffer@gmail.com>
2024-08-01 00:05:56 +0200

    Contact page now connects with sanity, awaiting contact card component

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,7 +1,27 @@
+import { sanityFetch } from "../../../lib/fetchData";
+import { SanityDocument } from "next-sanity";
+
+interface ContactDetails extends SanityDocument {
+  image: string,
+  fname: string,
+  lname: string,
+  role: string,
+  company: string,
+  email: string,
+  mobile: string,
+}
+
 export default async function Kontakt() {
+  const QUERY = `*[_type == "contacts"]{image, fname, lname, role, company, email, mobile}`
+  const data = await sanityFetch<ContactDetails[]>({query: QUERY});
+
   return (
     <main className="flex flex-col items-center justify-between p-0">
-      ...
+      {data.map(contact => (
+        <div key={contact._id}>
+          {contact.fname} {contact.lname}
+        </div>
+      ))}
     </main>
   );
 }
