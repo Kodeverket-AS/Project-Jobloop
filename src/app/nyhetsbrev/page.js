e8b08c88ad8225c41925a1c1186df56a45a40d3d
AndyKodehode <andykodehode@gmail.com>
2024-08-06 16:40:01 +0200

    responsive

--- src/app/nyhetsbrev/page.js
+++ src/app/nyhetsbrev/page.js
@@ -14,14 +14,14 @@ export default function Nyhetsbrev({text, link, icon}) {
   return (
     <main className="w-full flex flex-col items-center justify-center min-h-screen p-0 pb-20">
       <div className="w-4/6 flex flex-col gap-10">
-        <div className="flex flex-row gap-10">
+        <div className="flex flex-col md:flex-row gap-10">
           
           <SocialMediaCard text="facebook" link="https://www.facebook.com/JobLoop.Norge/" icon={<FaFacebook className="w-16 h-16 fill-[#1877F2]" />} />
           <SocialMediaCard text="LinkedIn" link="https://www.linkedin.com/company/jobloop/" icon={<FaLinkedin className="w-16 h-16 fill-[#1877F2]" />}/>
          </div>
 
-         <div className="flex flex-row gap-10">
-          <SocialMediaCard text="Instagram" link ="https://www.instagram.com/jobloop.norge/" icon={<Image src={Instagram} className="w-16 h-16"/>}/>
+         <div className="flex flex-col md:flex-row gap-10">
+          <SocialMediaCard text="Instagram" link ="https://www.instagram.com/jobloop.norge/" icon={<Image src={Instagram} className="min-w-16 min-h-16 max-w-16 max-h-16"/>}/>
           <SocialMediaCard text="Twitter" icon={<FaTwitter className="w-16 h-16 fill-[#1877F2]" />}/>
         </div>
       </div>
