791df9554542fa3604f5dc0433c778fb1d7334f5
AndyKodehode <andykodehode@gmail.com>
2024-08-06 15:07:32 +0200

    padding bottom

--- src/app/nyhetsbrev/page.js
+++ src/app/nyhetsbrev/page.js
@@ -10,18 +10,18 @@ import { SocialMediaCard } from "../../../components/socialMediaCard";
 export default function Nyhetsbrev({text, link, icon}) {
   
   return (
-    <main className="w-full flex flex-col items-center justify-center min-h-screen p-0">
+    <main className="w-full flex flex-col items-center justify-center min-h-screen p-0 pb-20">
       <div className="w-4/6 flex flex-col gap-10">
-       <div className="flex flex-row gap-10">
-        
-        <SocialMediaCard text="facebook" link="https://www.facebook.com/JobLoop.Norge/" icon={<FaFacebook className="w-12 h-12" />} />
-        <SocialMediaCard text="LinkedIn" link="https://www.linkedin.com/company/jobloop/" icon={<FaLinkedin className="w-12 h-12" />}/>
-      </div>
+        <div className="flex flex-row gap-10">
+          
+          <SocialMediaCard text="facebook" link="https://www.facebook.com/JobLoop.Norge/" icon={<FaFacebook className="w-12 h-12" />} />
+          <SocialMediaCard text="LinkedIn" link="https://www.linkedin.com/company/jobloop/" icon={<FaLinkedin className="w-12 h-12" />}/>
+         </div>
 
-      <div className="flex flex-row gap-10">
-        <SocialMediaCard text="Instagram" link ="https://www.instagram.com/jobloop.norge/" icon={<FaInstagram className="w-12 h-12" />}/>
-        <SocialMediaCard text="Twitter" icon={<FaTwitter className="w-12 h-12" />}/>
-      </div>
+         <div className="flex flex-row gap-10">
+          <SocialMediaCard text="Instagram" link ="https://www.instagram.com/jobloop.norge/" icon={<FaInstagram className="w-12 h-12" />}/>
+          <SocialMediaCard text="Twitter" icon={<FaTwitter className="w-12 h-12" />}/>
+        </div>
       </div>
     </main>
   );
