eb023f0b9cc9f31a3441c052d6f8019de9023235
AndyKodehode <andykodehode@gmail.com>
2024-08-06 16:32:43 +0200

    farger lagt inn

--- src/app/nyhetsbrev/page.js
+++ src/app/nyhetsbrev/page.js
@@ -5,7 +5,9 @@ import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import LinkedIn from "../../../public/LinkedIn.png"
 import Facebook2 from "../../../public/Facebook2.svg"
+import Instagram from "../../../public/Instagram.svg"
 import { SocialMediaCard } from "../../../components/socialMediaCard";
+
 
 export default function Nyhetsbrev({text, link, icon}) {
   
@@ -14,15 +16,15 @@ export default function Nyhetsbrev({text, link, icon}) {
       <div className="w-4/6 flex flex-col gap-10">
         <div className="flex flex-row gap-10">
           
-          <SocialMediaCard text="facebook" link="https://www.facebook.com/JobLoop.Norge/" icon={<FaFacebook className="w-12 h-12" />} />
-          <SocialMediaCard text="LinkedIn" link="https://www.linkedin.com/company/jobloop/" icon={<FaLinkedin className="w-12 h-12" />}/>
+          <SocialMediaCard text="facebook" link="https://www.facebook.com/JobLoop.Norge/" icon={<FaFacebook className="w-16 h-16 fill-[#1877F2]" />} />
+          <SocialMediaCard text="LinkedIn" link="https://www.linkedin.com/company/jobloop/" icon={<FaLinkedin className="w-16 h-16 fill-[#1877F2]" />}/>
          </div>
 
          <div className="flex flex-row gap-10">
-          <SocialMediaCard text="Instagram" link ="https://www.instagram.com/jobloop.norge/" icon={<FaInstagram className="w-12 h-12" />}/>
-          <SocialMediaCard text="Twitter" icon={<FaTwitter className="w-12 h-12" />}/>
+          <SocialMediaCard text="Instagram" link ="https://www.instagram.com/jobloop.norge/" icon={<Image src={Instagram} className="w-16 h-16"/>}/>
+          <SocialMediaCard text="Twitter" icon={<FaTwitter className="w-16 h-16 fill-[#1877F2]" />}/>
         </div>
       </div>
     </main>
-  );
+  )
 }
