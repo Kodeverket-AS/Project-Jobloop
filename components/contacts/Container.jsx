81cbab84903adbb2fb7b99c9ce2380e739da6936
kristoffer <kunkristoffer@gmail.com>
2024-08-06 18:02:31 +0200

    Adding message for no results and tweaking styles

--- components/contacts/Container.jsx
+++ components/contacts/Container.jsx
@@ -24,15 +24,13 @@ export default function ContactContainer({ data }) {
 
   return (
     <div className="flex flex-col gap-24 pb-24">
-      <div className="flex bg-green-700">
-        <span className="flex flex-col">
-          <label htmlFor="">Søk etter ansatt</label>
-          <input type="text" onChange={e => searchName(e.target.value.toLowerCase())} />
+      <div className="grid grid-cols-2 gap-4">
+        <span>
+          <input className="w-full text-lg p-[10px] border focus:border-jobloop-primary-green focus:outline-none" type="text" placeholder="Søk etter ansatt ..." onChange={e => searchName(e.target.value.toLowerCase())} />
         </span>
-        <span className="flex flex-col">
-          <label htmlFor="">Avdeling</label>
-          <select defaultValue="" onChange={e => searchDepartment(e.target.value.toLowerCase())}>
-            <option value="">Alle</option>
+        <span>
+          <select className="w-full text-lg p-[12px] border bg-white focus:border-jobloop-primary-green focus:outline-none" defaultValue="" onChange={e => searchDepartment(e.target.value.toLowerCase())}>
+            <option value="">Alle avdelinger</option>
             <option value="gamify">Gamify</option>
             <option value="kodehode">Kodehode</option>
             <option value="jobloop">Jobloop</option>
@@ -41,9 +39,14 @@ export default function ContactContainer({ data }) {
       </div>
 
       <div className="flex flex-col gap-12">
-        {filtered.map((contact, index) => (
-          <ContactCard key={index} contact={contact} />
-        ))}
+        {
+          filtered.length > 0 ?
+          filtered.map((contact, index) => (
+            <ContactCard key={index} contact={contact} />
+          ))
+          :
+          <span className="text-center">Beklager, ditt søk ga ingen resultater</span>
+        }
       </div>
     </div>
   );
