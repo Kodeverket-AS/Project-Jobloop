a9620fa8a28877f96da76957d1e6b3dff147c7ee
kristoffer <kunkristoffer@gmail.com>
2024-08-06 13:39:49 +0200

    Converting file extension to follow project guidelines

--- src/app/kontakt/page.jsx
+++ src/app/kontakt/page.jsx
@@ -1,20 +1,9 @@
 import { sanityFetch } from "../../../lib/fetchData";
-import { SanityDocument } from "next-sanity";
 import ContactCard from "../../../components/contacts/Card";
-
-export interface ContactDetails extends SanityDocument {
-  image: string,
-  fname: string,
-  lname: string,
-  role: string,
-  company: string,
-  email: string,
-  mobile: string,
-}
 
 export default async function Kontakt() {
   const QUERY = `*[_type == "contacts"]{"image": image.asset->url, fname, lname, role, company, email, mobile}`
-  const data = await sanityFetch<ContactDetails[]>({query: QUERY});
+  const data = await sanityFetch({query: QUERY});
 
   return (
     <main className="flex flex-col items-center justify-between py-24">
