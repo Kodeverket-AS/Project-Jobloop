0c5ad8e58ce9ec32e38cb8467fbee4625ec9ac83
AndyKodehode <andykodehode@gmail.com>
2024-08-07 11:15:12 +0200

    STOR BOKSTAV

--- src/app/nyhetsbrev/page.js
+++ src/app/nyhetsbrev/page.js
@@ -18,7 +18,7 @@ export default function Nyhetsbrev({text, link, icon}) {
       <div className="w-4/6 flex flex-col gap-10">
         <div className="flex flex-col md:flex-row gap-10">
           
-          <SocialMediaCard text="facebook" link="https://www.facebook.com/JobLoop.Norge/" icon={<FaFacebook className="w-16 h-16 fill-[#1877F2]" />} />
+          <SocialMediaCard text="Facebook" link="https://www.facebook.com/JobLoop.Norge/" icon={<FaFacebook className="w-16 h-16 fill-[#1877F2]" />} />
           <SocialMediaCard text="LinkedIn" link="https://www.linkedin.com/company/jobloop/" icon={<FaLinkedin className="w-16 h-16 fill-[#1877F2]" />}/>
          </div>
 
