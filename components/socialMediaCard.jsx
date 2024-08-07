937f441ea6e50df9f574be427ade7349eb9be968
AndyKodehode <andykodehode@gmail.com>
2024-08-07 14:39:58 +0200

    responsive fix

--- components/socialMediaCard.jsx
+++ components/socialMediaCard.jsx
@@ -11,8 +11,8 @@ export function SocialMediaCard({text, link, icon}){
 
 
     return(
-      <a href={link}className="w-full">
-       <div className="w-full flex flex-row p-12 gap-10 rounded-md shadow-lg items-center">
+      <a href={link}className="w-full flex justify-center">
+       <div className="w-full  flex flex-row p-12 gap-10 rounded-md shadow-lg items-center ">
          <div>
          {icon}
             {/* <Image src={icon} className="max-w-24"/> */}
--- src/app/nyhetsbrev/page.js
+++ src/app/nyhetsbrev/page.js
@@ -12,20 +12,14 @@ import { SocialMediaCard } from "../../../components/socialMediaCard";
 export default function Nyhetsbrev({text, link, icon}) {
   
   return (
-    <main className="w-full flex flex-col items-center justify-center min-h-screen p-0 pb-24">
+    <main className="w-full flex flex-col items-center justify-center min-h-screen p-0 pb-24 pt-20 lg:pt-0">
       
       <h1 className="pb-12 text-jobloop-primary-grey">Sosiale medier</h1>
-      <div className="w-4/6 flex flex-col gap-10">
-        <div className="flex flex-col md:flex-row gap-10">
+      <div className="w-full flex flex-col lg:flex-row gap-10 p-16 justify-center">
           
           <SocialMediaCard text="Facebook" link="https://www.facebook.com/JobLoop.Norge/" icon={<FaFacebook className="w-16 h-16 fill-[#1877F2]" />} />
           <SocialMediaCard text="LinkedIn" link="https://www.linkedin.com/company/jobloop/" icon={<FaLinkedin className="w-16 h-16 fill-[#1877F2]" />}/>
-         </div>
-
-         <div className="flex flex-col md:flex-row gap-10">
           <SocialMediaCard text="Instagram" link ="https://www.instagram.com/jobloop.norge/" icon={<Image src={Instagram} className="min-w-16 min-h-16 max-w-16 max-h-16"/>}/>
-          <SocialMediaCard text="Twitter" icon={<FaTwitter className="w-16 h-16 fill-[#1877F2]" />}/>
-        </div>
       </div>
     </main>
   )
