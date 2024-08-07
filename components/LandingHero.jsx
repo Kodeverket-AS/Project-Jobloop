e19456ce409756179fa7332aebb9de64bcf62dfd
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-08-07 10:08:14 +0200

    Adjusted text box to be wider.

--- components/LandingHero.jsx
+++ components/LandingHero.jsx
@@ -9,7 +9,7 @@ import { LinkButtonRed } from "./Buttons";
 export default function LandingHero() {
   return (
     <>
-      <div className="w-full h-3/4 relative">
+      <div className="w-full h-4/5 relative overflow-hidden">
         <div className="absolute top-0 left-0 flex justify-center w-full h-full -z-50 lg:h-[34rem] 2xl:h-[50rem]">
           <div className="absolute bg-gradient-to-b from-[rgba(34,34,34,0.8)] to-[rgba(34,34,34,0.5)] z-50 h-full w-full" />
           <Image
@@ -17,10 +17,10 @@ export default function LandingHero() {
             width={2000}
             alt={"people talking"}
             src={LandingHeaderPhoto}
-            className="object-cover w-full h-full"
+            className="object-cover object-bottom w-full h-full"
           />
         </div>
-        <div className=" flex flex-col gap-4 md:gap-6 items-start max-w-md xl:max-w-xl 2xl:max-w-2xl pl-8 md:pl-16 py-32 lg:py-32 2xl:py-40">
+        <div className=" h-full flex flex-col gap-4 md:gap-6 items-start max-w-md lg:max-w-xl 2xl:max-w-4xl pl-8 pr-8 md:pr-0 md:pl-16 py-32 lg:py-32 2xl:py-40">
           <div className="pb-4 text-left">
             <h1 className="pb-4 text-3xl xs:text-4xl text-kv-white  lg:text-4xl xl:text-5xl 2xl:text-6xl">
               Inkluderende arbeidspraksis og effektiv jobbmatching
