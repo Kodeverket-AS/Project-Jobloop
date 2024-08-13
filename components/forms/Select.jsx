fede712451505ad8ae04ff91d2245b048c577952
kristoffer <kunkristoffer@gmail.com>
2024-08-13 18:09:36 +0200

    Defining variables and base struc

--- components/forms/Select.jsx
+++ components/forms/Select.jsx
@@ -1,3 +1,22 @@
+"use client"
+
+import { useRef, useState } from "react";
+
 export default function Select({}) {
-  //
+  const [ isOpen, setIsOpen ] = useState(false)
+  const [ selected, setSelected ] = useState(false)
+  const options = useRef([])
+  const input = useRef()
+  const type = useRef("text")
+
+  return (
+    <div>
+      <input type={type} ref={input} />
+      <ul>
+        {options.current.map((option, index) =>
+          <li key={index}>{option}</li>
+        )}
+      </ul>
+    </div>
+  )
 }
