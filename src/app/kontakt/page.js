3c0dabc29cf5698f4b922f009b1eaada16758bca
AndreasJL86 <andreas@jobloop.no>
2025-02-04 13:51:00 +0100

    change the size of the person img

--- src/app/kontakt/page.js
+++ src/app/kontakt/page.js
@@ -2,12 +2,12 @@ import { sanityFetch } from "../../../lib/fetchData";
 import ContactContainer from "../../../components/contacts/Container";
 
 export default async function Kontakt() {
-  const QUERY = `*[_type == "contacts"]{..., "image": image.asset->url}| order(lname asc) | order(priority asc)`
-  const data = await sanityFetch({query: QUERY});
+    const QUERY = `*[_type == "contacts"]{..., "image": image.asset->url}| order(lname asc) | order(priority asc)`;
+    const data = await sanityFetch({ query: QUERY });
 
-  return (
-    <main className="flex flex-col items-center justify-between">
-      <ContactContainer data={data} />
-    </main>
-  );
+    return (
+        <main className='flex flex-col items-center justify-between'>
+            <ContactContainer data={data} />
+        </main>
+    );
 }
