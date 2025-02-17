ffd7945449ea1ae3bb2e1ae50e8dcffa6c248b13
RasmusKodehode <rasmus.kodehode@gmail.com>
2025-02-17 12:04:36 +0100

    added revalidate in getData

--- lib/fetchData.tsx
+++ lib/fetchData.tsx
@@ -5,6 +5,6 @@ export async function sanityFetch<QueryResponse>({ query }: { query: string;}) {
   return Client.fetch<QueryResponse>(query, {}, {
     next: {
       revalidate: process.env.NODE_ENV === 'development' ? 10 : 12
-    }
+    },
   });
 }
