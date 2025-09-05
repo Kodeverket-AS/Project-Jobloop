0111f345499cc6828c908b7db14851faf790c07c
Monika <maam092024@gmail.com>
2025-09-05 13:24:13 +0200

    hero w-full

--- components/LandingHero.jsx
+++ components/LandingHero.jsx
@@ -13,25 +13,23 @@ import {
 export default function LandingHero() {
   return (
     <>
-      <div className="w-full h-[85vh] sm:h-[80vh] relative">
-        <div className="absolute top-0 left-0 flex justify-center w-full h-full -z-50 ">
+      <div className="w-full h-[45vh]  lg:-[50vh] 2xl:h-[60vh] min-h-[500px]  lg:min-h-[700px] relative mb-16">
+        <div className="absolute top-0 left-0 w-full h-full -z-50">
           <div className="absolute bg-gradient-to-b from-[rgba(34,34,34,0.7)] to-[rgba(34,34,34,0.6)] z-50 h-full w-full" />
           <Image
-            height={500}
-            width={2000}
             alt={"people talking"}
             src={LandingHeaderPhoto}
             className="object-cover object-bottom w-full h-full"
+            fill
           />
         </div>
 
-        <div className="w-full h-full bg-neutral-800/20 backdrop-blur-sm mx-auto justify-center flex flex-col gap-4 md:gap-6 items-center px-4 py-32 lg:py-48">
-          <div className="pb-4 text-center max-w-3xl">
-            <h1 className="pb-6 text-3xl xs:text-4xl text-kv-white lg:text-7xl ">
-              {/* Inkluderende arbeidspraksis og effektiv jobbmatching */}
+        <div className="w-full h-full bg-neutral-800/20 backdrop-blur-sm mx-auto justify-center flex flex-col gap-6 md:gap-8 lg:gap-10 items-center px-6 md:px-8 lg:px-12 py-16 md:py-24 lg:py-32 xl:py-40 relative z-10">
+          <div className="text-center max-w-4xl">
+            <h1 className="mb-6 md:mb-8 text-2xl xs:text-3xl  md:text-5xl lg:text-6xl xl:text-7xl text-kv-white leading-tight">
               Nye veier til utvikling, utdanning og jobb!
             </h1>
-            <p className="font-normal text-base md:text-lg text-kv-white lg:text-2xl">
+            <p className="font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-kv-white leading-relaxed">
               Vi hjelper deg med inkluderende arbeidsmuligheter og alternativ
               opplæring. Sammen kan vi skape muligheter for fler!
             </p>
--- src/app/page.js
+++ src/app/page.js
@@ -9,15 +9,16 @@ import ContactComponent from "../../components/ContactComponent";
 
 export default function Home() {
     return (
-        <main className='flex flex-col items-center gap-16 w-full max-w-[1536px] mx-auto mb-16'>
+        <>
             <LandingHero />
+            <main className='flex flex-col items-center gap-16 w-full max-w-[1536px] mx-auto mb-16'>
                 <LandingIntro />
                 <SchoolCourses />
                 <WorkCourses />
                 <CooperationProjects />
                 <JobloopSuccess />
                 <ContactComponent />
-           
-        </main>
+            </main>
+        </>
     );
 }
