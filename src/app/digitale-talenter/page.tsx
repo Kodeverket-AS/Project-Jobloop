f72777ac812c24981f23105c16675fed399cedd1
Kristoffer <kunkristoffer@gmail.com>
2025-10-16 12:56:56 +0200

    converted digitale talenter page to tsx

--- src/app/digitale-talenter/page.tsx
+++ src/app/digitale-talenter/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { getData } from '../../../lib/GetData';
+import { getData } from '@/lib/GetData';
 import HeroSub from '@/components/herosub';
 import CourseSummary from '@/components/CourseSummary';
 import Curriculum from '@/components/Curriculum';
@@ -8,14 +8,16 @@ import LeaderSection from '@/components/LeaderSection';
 import ForCompanies from '@/components/ForCompanies';
 import ForSchool from '@/components/ForSchool';
 import Places from '@/components/Places';
+import { Tiltak } from '@/types/sanity/sanity.types';
 
 export default async function DigitaleTalenter() {
   const data = await getData();
-  const digTal = data.tiltak[2];
+  const digTal = data[2];
+
   return (
     <main className='flex flex-col items-center gap-12 w-full max-w-[1536px] my-12 mx-auto'>
       <HeroSub content={digTal} />
-      <CourseSummary content={digTal} course={'Digitale Talenter'} />
+      <CourseSummary {...digTal} />
       <div className='w-full max-w-4xl px-4'>
         <div className='relative rounded-xl overflow-hidden shadow-2xl bg-white/5 backdrop-blur-xs aspect-video animate-fade-in ring-1 ring-black/10'>
           <video
@@ -30,27 +32,8 @@ export default async function DigitaleTalenter() {
           </video>
         </div>
       </div>
-      <Curriculum content={digTal} course={'Digitale Talenter'} />
-      <LeaderSection
-        leaders={[
-          {
-            name: 'Frode Moldestad Bøe',
-            position: 'Kursleder, Digitale Talenter',
-            image: '/frode.webp',
-            description: [
-              {
-                _type: 'block',
-                children: [
-                  {
-                    _type: 'span',
-                    text: 'Frode Moldestad Bøe\nfrode@jobloop.no\n+47 555 12 345',
-                  },
-                ],
-              },
-            ],
-          },
-        ]}
-      />
+      <Curriculum title={digTal.title} curriculum={digTal.curriculum} />
+      <LeaderSection leaders={digTal.courseLeaders} />
     </main>
   );
 }
