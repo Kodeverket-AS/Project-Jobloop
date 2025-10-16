e36fb7b7b794489f0d7b9ace61133ee1e12e8919
Kristoffer <kunkristoffer@gmail.com>
2025-10-16 13:16:06 +0200

    updated imports in digitale talenter page

--- src/app/digitale-talenter/page.tsx
+++ src/app/digitale-talenter/page.tsx
@@ -1,22 +1,19 @@
-import Image from 'next/image';
 import { getData } from '@/lib/GetData';
 import HeroSub from '@/components/herosub';
 import CourseSummary from '@/components/CourseSummary';
 import Curriculum from '@/components/Curriculum';
-import CourseUsefullness from '@/components/CourseUsefulness';
 import LeaderSection from '@/components/LeaderSection';
-import ForCompanies from '@/components/ForCompanies';
-import ForSchool from '@/components/ForSchool';
-import Places from '@/components/Places';
-import { Tiltak } from '@/types/sanity/sanity.types';
 
 export default async function DigitaleTalenter() {
   const data = await getData();
-  const digTal = data[2];
+  const digTal = data.tiltak.at(2);
+
+  // todo
+  if (!digTal) return null;
 
   return (
     <main className='flex flex-col items-center gap-12 w-full max-w-[1536px] my-12 mx-auto'>
-      <HeroSub content={digTal} />
+      <HeroSub {...digTal} />
       <CourseSummary {...digTal} />
       <div className='w-full max-w-4xl px-4'>
         <div className='relative rounded-xl overflow-hidden shadow-2xl bg-white/5 backdrop-blur-xs aspect-video animate-fade-in ring-1 ring-black/10'>
@@ -32,7 +29,7 @@ export default async function DigitaleTalenter() {
           </video>
         </div>
       </div>
-      <Curriculum title={digTal.title} curriculum={digTal.curriculum} />
+      <Curriculum title='Digitale Talenter' curriculum={digTal.curriculum} />
       <LeaderSection leaders={digTal.courseLeaders} />
     </main>
   );
