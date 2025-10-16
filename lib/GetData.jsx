95b060d189c8b12fda488ab1a19afafdbd29c5e8
Kristoffer <kunkristoffer@gmail.com>
2025-10-16 12:18:17 +0200

    tweaked existing getData to stop converting image urls in fetch and instead let urlBuilder handle implementation

--- lib/GetData.jsx
+++ lib/GetData.jsx
@@ -1,8 +1,36 @@
-import { Client } from "./Sanity";
+import { Client } from './Sanity';
 
 export async function getData() {
   return Client.fetch(`{
-    "tiltak": *[_type == "tiltak"] | order(index) {title, heading, introtext, "image":image.asset->url, "alt":image.alt, targetGroup, targetGroupShort, duration, lectureType, locations, curriculum, use, leader, courseLeaders, "image2":image2.asset->url, "alt2":image2.alt, employer, "image3":image3.asset->url, "alt3":image3.alt, school, "image4":image4.asset->url, "alt4":image4.alt, cities, FAQ1, FAA1, FAQ2, FAA2, FAQ3, FAA3, index, _id},
+    "tiltak": *[_type == "tiltak"] | order(index) {
+      title, 
+      heading, 
+      introtext, 
+      image, 
+      targetGroup, 
+      targetGroupShort, 
+      duration,
+      lectureType,
+      locations,
+      curriculum,
+      use,
+      leader,
+      courseLeaders,
+      image2,
+      employer,
+      image3,
+      school,
+      image4,
+      cities,
+      FAQ1,
+      FAA1,
+      FAQ2,
+      FAA2,
+      FAQ3,
+      FAA3,
+      index,
+      _id
+    },                  
   }`);
   //{ cache: "no-cache" }
 }
