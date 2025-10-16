6c302c3e373a2ab07381d9d6e21ec2d5db05a9c6
Kristoffer <kunkristoffer@gmail.com>
2025-10-16 14:30:41 +0200

    converted contact page into tsx

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,11 +1,12 @@
-import { sanityFetch } from '../../../lib/fetchData';
+import type { Contacts } from '@/types/sanity/sanity.types';
+import { sanityFetch } from '@/lib/fetchData';
 import ContactContainer from '@/components/contacts/Container';
 
 export const revalidate = 60;
 
 export default async function Kontakt() {
   const QUERY = `*[_type == "contacts"]{..., "image": image.asset->url} | order(priority asc, lname asc)`;
-  const data = await sanityFetch({ query: QUERY });
+  const data = await sanityFetch<Contacts[]>({ query: QUERY });
 
   return (
     <main className='mx-auto w-full max-w-[1536px] px-4 sm:px-6 lg:px-8 py-10'>
