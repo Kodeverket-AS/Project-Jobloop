e39c44e5278accf3c2ab119689535df6cea56104
Kristoffer <kunkristoffer@gmail.com>
2025-10-18 17:03:21 +0200

    Implemented next-intl plugin in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from 'next';
+import { type NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
-export default {
+// NextJS configuration
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'cdn.sanity.io' }],
   },
-} satisfies NextConfig;
+};
+
+// Next-intl settings
+const withNextIntl = createNextIntlPlugin();
+
+// Let Next-intl integrate with NextJS
+export default withNextIntl(nextConfig);
