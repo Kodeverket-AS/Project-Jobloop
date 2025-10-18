6b5c5e5e2b40670d4ca8d1383c973e778a88e758
Kristoffer <kunkristoffer@gmail.com>
2025-10-18 17:06:11 +0200

    added request function for fetching localization in next-intl

---
+++ src/i18n/request.ts
@@ -0,0 +1,22 @@
+import { hasLocale } from 'next-intl';
+import { getRequestConfig } from 'next-intl/server';
+import { routing } from './routing';
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale;
+
+  // Import required localizations for routes, look into route specific for reducing build times
+  const metadata = (await import(`../translations/${locale}/metadata.json`))
+    .default;
+  const header = (await import(`../translations/${locale}/header.json`))
+    .default;
+  const cta = (await import(`../translations/${locale}/cta.json`)).default;
+
+  return {
+    locale,
+    messages: { metadata, header, cta },
+  };
+});
