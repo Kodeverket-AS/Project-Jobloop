57810582728c30a82e57d49710cd3d86b76e9b5e
Kristoffer <kunkristoffer@gmail.com>
2025-10-18 17:07:41 +0200

    added translations to hero section

--- src/components/LandingHero.tsx
+++ src/components/LandingHero.tsx
@@ -1,10 +1,14 @@
-'use client';
-
 import Image from 'next/image';
-import LandingHeaderPhoto from '../../public/LandingHeaderPhoto.png';
-import { LinkButtonAnimatedWithIcon, LinkButtonAnimated } from './buttons';
+import LandingHeaderPhoto from '@public/LandingHeaderPhoto.png';
+import {
+  LinkButtonAnimatedWithIcon,
+  LinkButtonAnimated,
+} from '@/components/buttons';
+import { getTranslations } from 'next-intl/server';
 
-export default function LandingHero() {
+export default async function LandingHero() {
+  const t = await getTranslations('header');
+
   return (
     <>
       <div className='w-full h-[45vh]  lg:-[50vh] 2xl:h-[60vh] min-h-[500px]  lg:min-h-[700px] relative mb-16'>
@@ -17,23 +21,21 @@ export default function LandingHero() {
             fill
           />
         </div>
-
         <div className='w-full h-full bg-neutral-800/20 backdrop-blur-xs mx-auto justify-center flex flex-col gap-6 md:gap-8 lg:gap-10 items-center px-6 md:px-8 lg:px-12 py-16 md:py-24 lg:py-32 xl:py-40 relative z-10'>
           <div className='text-center max-w-4xl'>
             <h1 className='mb-6 md:mb-8 text-2xl xs:text-3xl  md:text-5xl lg:text-6xl xl:text-7xl text-kv-white leading-tight'>
-              Nye veier til utvikling, utdanning og jobb!
+              {t('title')}
             </h1>
             <p className='font-normal text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-kv-white leading-relaxed'>
-              Vi hjelper deg med inkluderende arbeidsmuligheter og alternativ
-              opplæring. Sammen kan vi skape muligheter for fler!
+              {t('subtitle')}
             </p>
           </div>
           <div className='w-full flex flex-col gap-4 sm:flex-row justify-center'>
             <LinkButtonAnimatedWithIcon
               Path={'/kontakt'}
-              Text={'Kontakt Oss'}
+              Text={t('cta.contactUs')}
             />
-            <LinkButtonAnimated Path={'#courses'} Text={'Våre tilbud'} />
+            <LinkButtonAnimated Path={'#courses'} Text={t('cta.courses')} />
           </div>
         </div>
       </div>
