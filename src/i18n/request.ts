bbe822a6c2f3247f948363e4f3994d59837c7de8
Kristoffer <kunkristoffer@gmail.com>
2025-10-20 20:37:50 +0200

    added more localization files to i18n

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,19 +4,17 @@ import { routing } from './routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
-  const locale = hasLocale(routing.locales, requested)
-    ? requested
-    : routing.defaultLocale;
+  const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
 
   // Import required localizations for routes, look into route specific for reducing build times
-  const metadata = (await import(`../translations/${locale}/metadata.json`))
-    .default;
-  const header = (await import(`../translations/${locale}/header.json`))
-    .default;
+  // const metadata = (await import(`../translations/${locale}/metadata.json`)).default;
+  const base = (await import(`../translations/${locale}/header.json`)).default;
+  const header = (await import(`../translations/${locale}/header.json`)).default;
   const cta = (await import(`../translations/${locale}/cta.json`)).default;
+  const errors = (await import(`../translations/${locale}/errors.json`)).default;
 
   return {
     locale,
-    messages: { metadata, header, cta },
+    messages: { base, header, cta, errors },
   };
 });
