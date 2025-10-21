1bab51fc86b7f18ccc10f8e64e8a9c39b1c809ab
Kristoffer <kunkristoffer@gmail.com>
2025-10-21 15:15:48 +0200

    updated loop page with new static version using new sanity fetch

--- src/app/[locale]/loop/page.tsx
+++ src/app/[locale]/loop/page.tsx
@@ -1,16 +1,22 @@
-import { getData } from '@/lib/GetData';
+import { getTiltakById, getTiltakByIdLocalized } from '@/lib/sanity/fetch';
+import { notFound } from 'next/navigation';
+import { AvailableLocales } from '@/i18n/routing';
 import HeroSub from '@/components/herosub';
 
-export default async function Loop() {
-  const data = await getData();
-  const loop = data.tiltak.at(7);
+export async function generateStaticParams() {
+  const result = await getTiltakById({ index: 10 });
+  return result.map((tiltak) => ({ locale: tiltak.language ?? '' }));
+}
 
-  // todo
-  if (!loop) return null;
+export default async function Loop({ params }: { params: Promise<{ locale: string }> }) {
+  // Check that page exists in Sanity with correct locale
+  const locale = (await params).locale;
+  const tiltak = await getTiltakByIdLocalized({ index: 10, locale: locale as AvailableLocales });
+  if (!tiltak) return notFound();
 
   return (
     <main className='flex flex-col items-center gap-16 w-full max-w-[1536px] mx-auto my-16'>
-      <HeroSub {...loop} />
+      <HeroSub {...tiltak} />
     </main>
   );
 }
