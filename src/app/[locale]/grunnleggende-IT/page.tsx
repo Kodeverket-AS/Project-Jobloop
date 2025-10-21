e88aeff894a586a7e8daef75286f6d3e24f101e3
Kristoffer <kunkristoffer@gmail.com>
2025-10-21 14:58:29 +0200

    updated grunnleggende it with new static version using new sanity lib

--- src/app/[locale]/grunnleggende-IT/page.tsx
+++ src/app/[locale]/grunnleggende-IT/page.tsx
@@ -1,22 +1,28 @@
-import { getData } from '@/lib/GetData';
+import { getTiltakById, getTiltakByIdLocalized } from '@/lib/sanity/fetch';
+import { AvailableLocales } from '@/i18n/routing';
+import { notFound } from 'next/navigation';
+import LeaderSection from '@/components/LeaderSection';
 import HeroSub from '@/components/herosub';
 import CourseSummary from '@/components/CourseSummary';
 import Curriculum from '@/components/Curriculum';
-import LeaderSection from '@/components/LeaderSection';
 
-export default async function GrunnleggendeIT() {
-  const data = await getData();
-  const gIT = data.tiltak.at(3);
+export async function generateStaticParams() {
+  const result = await getTiltakById({ index: 3 });
+  return result.map((tiltak) => ({ locale: tiltak.language ?? '' }));
+}
 
-  // todo litt av noen variabel navn
-  if (!gIT) return null;
+export default async function GrunnleggendeIT({ params }: { params: Promise<{ locale: string }> }) {
+  // Check that page exists in Sanity with correct locale
+  const locale = (await params).locale;
+  const tiltak = await getTiltakByIdLocalized({ index: 3, locale: locale as AvailableLocales });
+  if (!tiltak) return notFound();
 
   return (
     <main className='flex flex-col items-center gap-16 w-full max-w-[1536px] mx-auto my-16'>
-      <HeroSub {...gIT} />
-      <CourseSummary {...gIT} />
-      <Curriculum title={'Grunnleggende IT'} curriculum={gIT.curriculum} />
-      <LeaderSection leaders={gIT.courseLeaders} />
+      <HeroSub {...tiltak} />
+      <CourseSummary {...tiltak} />
+      <Curriculum title={'Grunnleggende IT'} curriculum={tiltak.curriculum} />
+      <LeaderSection leaders={tiltak.courseLeaders} />
     </main>
   );
 }
