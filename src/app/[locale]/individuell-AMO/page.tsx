5ed230af7271bea957f0c7f1a08e607828ff5e29
Kristoffer <kunkristoffer@gmail.com>
2025-10-21 15:00:36 +0200

    updated individuell amo with new static version using new sanity lib

--- src/app/[locale]/individuell-AMO/page.tsx
+++ src/app/[locale]/individuell-AMO/page.tsx
@@ -1,15 +1,22 @@
-import { getData } from '@/lib/GetData';
+import { getTiltakById, getTiltakByIdLocalized } from '@/lib/sanity/fetch';
+import { AvailableLocales } from '@/i18n/routing';
+import { notFound } from 'next/navigation';
 import HeroSub from '@/components/herosub';
 
-export default async function IndividuellAMO() {
-  const data = await getData();
-  const amo = data.tiltak[5];
+export async function generateStaticParams() {
+  const result = await getTiltakById({ index: 5 });
+  return result.map((tiltak) => ({ locale: tiltak.language ?? '' }));
+}
 
-  if (!amo) return null;
+export default async function IndividuellAMO({ params }: { params: Promise<{ locale: string }> }) {
+  // Check that page exists in Sanity with correct locale
+  const locale = (await params).locale;
+  const tiltak = await getTiltakByIdLocalized({ index: 5, locale: locale as AvailableLocales });
+  if (!tiltak) return notFound();
 
   return (
     <main className='flex flex-col items-center gap-16 w-full max-w-[1536px] mx-auto my-16'>
-      <HeroSub {...amo} />
+      <HeroSub {...tiltak} />
     </main>
   );
 }
