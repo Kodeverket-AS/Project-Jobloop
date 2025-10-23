e763272a7bece4529b84880f63855f2c237b5e99
Kristoffer <kunkristoffer@gmail.com>
2025-10-23 10:47:55 +0200

    addding image to language dropdown

--- src/components/ui/menu/LocaleMenu.tsx
+++ src/components/ui/menu/LocaleMenu.tsx
@@ -1,16 +1,23 @@
 import { useLocale, useTranslations } from 'next-intl';
 import { routing } from '@/i18n/routing';
 import { LocaleMenuSelect } from './LocaleMenuSelect';
+import Image from 'next/image';
 
 export default function LocaleMenu() {
   const locale = useLocale();
-  const t = useTranslations("base.header.localeSwitcher");
+  const t = useTranslations('base.header.localeSwitcher');
 
   return (
     <LocaleMenuSelect defaultValue={locale}>
       {routing.locales.map((cur) => (
         <option key={cur} value={cur}>
-          {t('locales', { locale: cur })}
+          <Image
+              src={`/images/flags/${cur}.svg`}
+              height={20}
+              width={24}
+              alt={`Picture of ${t('locales', { locale: cur })}s flag`}
+            />
+          <span>{t('locales', { locale: cur })}</span>
         </option>
       ))}
     </LocaleMenuSelect>
