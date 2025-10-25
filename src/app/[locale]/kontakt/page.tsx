be98ccb6906f6492368d2f4528a9bc3c76e5baa1
Kristoffer <kunkristoffer@gmail.com>
2025-10-25 14:02:24 +0200

    removed old sanity lib refferences in favor of refactored version from last refactor

--- src/app/[locale]/kontakt/page.tsx
+++ src/app/[locale]/kontakt/page.tsx
@@ -1,12 +1,8 @@
-import type { Contacts } from '@/types/sanity/sanity.types';
-import { sanityFetch } from '@/lib/fetchData';
+import { getContacts } from '@/lib/sanity/fetch';
 import ContactContainer from '@/components/feature/contact/Container';
 
-export const revalidate = 60;
-
 export default async function Kontakt() {
-  const QUERY = `*[_type == "contacts"]{..., "image": image.asset->url} | order(priority asc, lname asc)`;
-  const data = await sanityFetch<Contacts[]>({ query: QUERY });
+  const data = await getContacts();
 
   return (
     <main className='mx-auto w-full max-w-[1536px] px-4 sm:px-6 lg:px-8 py-10'>
